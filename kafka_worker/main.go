12f1925f56e16ca3995e5c8c5bc6509b21847ae8
fceja <88167774+fceja@users.noreply.github.com>
2024-08-24 12:00:11 -0700

    add kafka work funcs

---
+++ kafka_worker/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/IBM/sarama"
+)
+
+type KafkaClient struct {
+	Brokers   []string
+	Config    *sarama.Config
+	Consumer  sarama.Consumer
+	TopicName string
+}
+
+type Message struct {
+	ID        int       `json:"id"`
+	Message   string    `json:"message"`
+	Name      string    `json:"name"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+// section: kafka funcs
+func newKafkaClient(brokers []string, topicName string) (*KafkaClient, error) {
+	// create sarama config
+	config := sarama.NewConfig()
+	config.Consumer.Group.Rebalance.Strategy = sarama.NewBalanceStrategyRoundRobin()
+
+	// create consumer
+	consumer, err := sarama.NewConsumer(brokers, config)
+	if err != nil {
+		log.Fatalf("failed to create consumer: %v", err)
+	}
+
+	return &KafkaClient{
+		Consumer:  consumer,
+		Config:    config,
+		Brokers:   brokers,
+		TopicName: topicName,
+	}, nil
+
+}
+
+func readMessagesFromTopic(client *KafkaClient) ([]Message, error) {
+	// consume topic partition
+	partition := int32(0)         // adjust if topic has multiple partitions
+	offset := sarama.OffsetOldest // start from the beginning (oldest)
+	partitionConsumer, err := client.Consumer.ConsumePartition(client.TopicName, partition, offset)
+	if err != nil {
+		return nil, fmt.Errorf("failed to consume partition: %w", err)
+	}
+	defer partitionConsumer.Close()
+
+	// retrieve messages from topic partition
+	var messages []Message
+	timeout := time.After(1 * time.Second) // Adjust the timeout as needed
+
+	for {
+		select {
+		case message, ok := <-partitionConsumer.Messages():
+			if !ok {
+				// channel closed
+				return messages, nil
+			}
+
+			// parse message to json
+			var msg Message
+			err := json.Unmarshal(message.Value, &msg)
+			if err != nil {
+				log.Printf("Failed to unmarshal message: %v", err)
+				continue
+			}
+
+			// store message
+			messages = append(messages, msg)
+		case <-timeout:
+			// timeout reached
+			log.Println("Timeout reached, stopping message retrieval.")
+			return messages, nil
+		}
+	}
+}
+
+// section: worker funcs
+func processMessage(messages []Message) {
+	log.Print("Messages:")
+	for _, msg := range messages {
+		log.Printf("ID=%d, Message=%s, Name=%s, Timestamp=%s", msg.ID, msg.Message, msg.Name, msg.Timestamp)
+	}
+
+	time.Sleep(1 * time.Second)
+}
+
+// section: main
+func main() {
+	// define broker (kafka docker container) and topic name
+	brokers := []string{"localhost:9092"}
+	topicName := "example_topic_2"
+
+	// create kafka client
+	client, err := newKafkaClient(brokers, topicName)
+	if err != nil {
+		log.Fatalf("Error creating kafka client: %v", err)
+	}
+
+	// retrieve messages from topic
+	messages, err := readMessagesFromTopic(client)
+	if err != nil {
+		log.Fatalf("Error retrieving messages: %v", err)
+	}
+
+	// process messages
+	processMessage(messages)
+
+	// close open connections
+	defer func() {
+		if client.Consumer != nil {
+			client.Consumer.Close()
+		}
+	}()
+}
