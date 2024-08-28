e2516ccef9ff7a68020864771fda37bdc550221d
fceja <88167774+fceja@users.noreply.github.com>
2024-08-27 23:28:54 -0700

    wip Dockerfile, and update to polling

--- kafka_producer/lib/kafka.go
+++ kafka_producer/lib/kafka.go
@@ -20,6 +20,22 @@ type Message struct {
 	Timestamp time.Time `json:"timestamp"`
 }
 
+func pollForKafkaConnection(brokerAddresses []string, config *sarama.Config) sarama.SyncProducer {
+	var producer sarama.SyncProducer
+
+	for {
+		var err error
+		producer, err = sarama.NewSyncProducer(brokerAddresses, config)
+		if err != nil {
+			zap.L().Error("Failed to start producer.", zap.Error(err))
+			time.Sleep(20 * time.Second)
+			continue
+		}
+		break
+	}
+	return producer
+}
+
 // adds message to topic
 func AddMessageToTopic(client *KafkaProducerClient, message Message, topicName string) error {
 	jsonMessage, err := json.Marshal(message)
@@ -31,7 +47,7 @@ func AddMessageToTopic(client *KafkaProducerClient, message Message, topicName s
 		Value: sarama.StringEncoder(jsonMessage),
 	})
 	if err != nil {
-		zap.L().Fatal("Failed to send message to kafka.", zap.Error(err))
+		zap.L().Error("Failed to send message to kafka.", zap.Error(err))
 	}
 
 	return nil
@@ -42,10 +58,7 @@ func InitKafkaProducerClient(brokerAddresses []string) (*KafkaProducerClient, er
 	config := sarama.NewConfig()
 	config.Producer.Return.Successes = true // ensure producer waits for acknowledgment
 
-	producer, err := sarama.NewSyncProducer(brokerAddresses, config)
-	if err != nil {
-		zap.L().Fatal("Failed to start producer.", zap.Error(err))
-	}
+	producer := pollForKafkaConnection(brokerAddresses, config)
 
 	return &KafkaProducerClient{
 		Config:   config,
